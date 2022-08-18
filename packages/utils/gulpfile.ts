065ed1dfc289e68260b1f4f3c25a9aeefd9f8154
dbx <duanbenxian@appiron.cn>
2022-08-18 16:48:06 +0800

    init

--- packages/utils/gulpfile.ts
+++
@@ -1,5 +0,0 @@
-
-import {buildPackages} from '../../build/packages'
-
-
-export default buildPackages(__dirname, 'utils');
--- play/app.vue
+++
@@ -1,9 +0,0 @@
-<!--
- * @Author: jing.chen
- * @Date: 2021-10-28 19:12:26
- * @LastEditors: 段本显
- * @LastEditTime: 2022-08-18 15:38:49
- * @Description: 
--->
-<template></template>
-<script lang="ts" setup></script>
